b789af901c5d82f4e5838860a3466b951d5e5447
Roman Gavino <romgavino@gmail.com>
2025-07-24 01:25:09 +0800

    Update authController.js

--- project-monitoring-interface-node/controllers/authController.js
+++ project-monitoring-interface-node/controllers/authController.js
@@ -54,7 +54,7 @@ const login = async (req, res) => {
         res.cookie("token", token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === "production",
-            sameSite: "Strict",
+            sameSite: "None",
             maxAge: 1800000, //30 min
         });
 
@@ -69,7 +69,7 @@ const logout = async (req, res) => {
         res.clearCookie("token", {
             httpOnly: true,
             secure: process.env.NODE_ENV === "production",
-            sameSite: "Strict",
+            sameSite: "None",
         });
 
         res.json({ message: "Logout successful" });
